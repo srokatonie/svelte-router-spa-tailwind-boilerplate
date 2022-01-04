0b3f5d6583cb1e1913b54c8fcb3bbf81021a736d
Maciej Sroka <srokatonie@gmail.com>
2022-01-04 22:00:50 +0100

    Layout fix

--- src/views/components/Layout.svelte
+++ src/views/components/Layout.svelte
@@ -9,7 +9,7 @@
   <TopHeader {currentRoute} />
   <section class="p-5 flex-1 flex-col grow">
     <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-      <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+      <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl sm:my-8 sm:align-middle sm:max-w-2xl sm:w-full">
         <div class="bg-white p-4">
           <Route {currentRoute} {params} />
         </div>
